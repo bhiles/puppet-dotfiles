b9e07ac9418e149d7570fbc45f19fc4f32f1dbd3
Bennett Hiles <bennetthiles@gmail.com>
2014-12-16 18:43:42 -0800

    emacs dotfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,5 +6,9 @@ class dotfiles {
       source   => 'git@github.com:bhiles/dotfiles.git',
       path     => '/Users/bennetthiles/.dotfiles',
       provider => 'git',
+    'emacs dotfiles':
+      source   => 'git@github.com:bhiles/.emacs.d.git',
+      path     => '/Users/bennetthiles/.emacs.d',
+      provider => 'git',      
   }
 }
