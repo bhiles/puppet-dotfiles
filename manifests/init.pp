9d160f1b1c2e36d17fbab32f0682f5f737f28dce
Bennett Hiles <bennetthiles@gmail.com>
2014-12-16 18:47:13 -0800

    add semicolon

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class dotfiles {
     'my dotfiles':
       source   => 'git@github.com:bhiles/dotfiles.git',
       path     => '/Users/bennetthiles/.dotfiles',
-      provider => 'git',
+      provider => 'git';
     'emacs dotfiles':
       source   => 'git@github.com:bhiles/.emacs.d.git',
       path     => '/Users/bennetthiles/.emacs.d',
