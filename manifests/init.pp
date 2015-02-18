50b8af909e2e2e48eec0596c32608923f6fc95d9
Bennett Hiles <bennetthiles@gmail.com>
2015-02-18 17:26:31 -0500

    new directory path

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,11 +4,11 @@ class dotfiles {
   repository {
     'my dotfiles':
       source   => 'git@github.com:bhiles/dotfiles.git',
-      path     => '/Users/bennetthiles/.dotfiles',
+      path     => '/Users/bennett/.dotfiles',
       provider => 'git';
     'emacs dotfiles':
       source   => 'git@github.com:bhiles/.emacs.d.git',
-      path     => '/Users/bennetthiles/.emacs.d',
+      path     => '/Users/bennett/.emacs.d',
       provider => 'git',      
   }
 }
